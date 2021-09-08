81a1cebd88b353b3fad5857c26953bb579c50c0b
iaiá <iaiaselva.is@gmail.com>
2021-09-08 10:31:35 -0300

    aula 11 - métodos put e delete

--- src/main/java/com/devdojo/essentials/controller/AnimeController.java
+++ src/main/java/com/devdojo/essentials/controller/AnimeController.java
@@ -5,12 +5,10 @@ import com.devdojo.essentials.service.AnimeService;
 import com.devdojo.essentials.utils.DateUtils;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.nio.file.Path;
 import java.time.LocalDateTime;
 import java.util.List;
 
@@ -29,7 +27,7 @@ public class AnimeController {
         return ResponseEntity.ok(animeService.listAll());
     }
 
-    @GetMapping(path = "/{id}")
+    @GetMapping("/{id}")
     public ResponseEntity<Anime> findById(@PathVariable long id){
         log.info(dateUtils.formatLocalDateTimeToDatabaseStyle(LocalDateTime.now())+" Retornando anime por ID");
         return ResponseEntity.ok(animeService.findById(id));
@@ -37,7 +35,19 @@ public class AnimeController {
 
     @PostMapping
     public ResponseEntity<Anime> save(@RequestBody Anime anime){
-        log.info("Cadastrando novo anime "+anime.getName());
+        log.info("Cadastrando novo anime: "+anime.getName());
         return new ResponseEntity<Anime>(animeService.save(anime),HttpStatus.CREATED);
+    }
+
+    @PutMapping
+    public ResponseEntity<Anime> replace (@RequestBody Anime anime){
+        animeService.replace(anime);
+        return new ResponseEntity<Anime>(anime, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity delete(@PathVariable long id){
+        animeService.delete(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 }
